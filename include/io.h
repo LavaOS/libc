edf3e7084e41050a536dacd11df405ba8a96a32f
amir <pare7amir7@gmail.com>
2025-08-13 21:11:20 +0330

    [io] fix

--- include/io.h
+++ include/io.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-static inline void outb(uint16_t port, uint8_t value);
-static inline void outw(uint16_t port, uint16_t value);
+void outb(uint16_t port, uint8_t value);
+void outw(uint16_t port, uint16_t value);
--- src/io.c
+++ src/io.c
@@ -1,9 +1,10 @@
 #include <stdint.h>
 #include <io.h>
 
-static inline void outb(uint16_t port, uint8_t value) {
+void outb(uint16_t port, uint8_t value) {
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
-static inline void outw(uint16_t port, uint16_t value) {
+
+void outw(uint16_t port, uint16_t value) {
     __asm__ volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
 }
