a8758af223716d7bac69d756db544ee469f9d47a
jrifuoue <pare7amir7@gmail.com>
2026-05-06 16:51:32 +0330

    dup2

--- include/unistd.h
+++ include/unistd.h
@@ -27,3 +27,4 @@ int execve(const char* pathname, char *const* argv, char *const* envp);
 int execvpe(const char* file, char* const* argv, char *const* envp); 
 int execv(const char* path, char* const* argv); 
 int execvp(const char* file, char* const* argv);
+int dup2(int oldfd, int newfd);
